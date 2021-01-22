548dc4d75f1832be7762d882f701cf4e6c7140b2
SHAILENDRA SETHIYA <shailendrasethiya@gmail.com>
2021-01-22 16:27:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
